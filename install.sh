75f73781b09f5e5d74abcba087d0033adbd899ba
Shane Lin <shane@ShanedeMacBook-Air.local>
2023-07-23 18:05:51 +0800

    * Refine install script

--- install.sh
+++ install.sh
@@ -9,6 +9,18 @@ check_folder_exist() {
     return 0
 }
 
+diff_files_folders() {
+    file1=$1
+    file2=$2
+    diff_result=$(diff -r "$file1" "$file2")
+    if [ -z "$diff_result" ]; then
+        echo "Files(folders) are identical. ([$file1] <-> [$file2])"
+        return 0
+    else
+        echo "Files(folders) are different. ([$file1] <-> [$file2])"
+        return 1
+    fi
+}
 mv_files() {
     filename=$1
     dest=$2
@@ -17,53 +29,88 @@ mv_files() {
     mv $filename $full_path/
 }
 
-backup_files() {
-    filename=$1
-    backup_folder=$2
-    mv_files $filename $backup_folder
-}
-
 copy_files() {
     filename=$1
     dest=$2
     full_path=$(realpath "$dest")
     printf "Copy [%s] to [%s]...\n" "$filename" "$full_path"
-    cp -r -v ./$filename $dest
+    cp -r -v $filename $dest
 }
 
+
+
 log_file="./build_log"
-
-exec > >(tee "$log_file") 2>&1
-
-files=(".vim" ".vimrc" ".tmux.conf" ".tmux_theme" ".clang-format" "bash_script" ".bash_profile" ".bash_commons")
+files=(".vim" ".vimrc" ".tmux.conf" ".tmux_theme" ".clang_format" "bash_script" ".bash_profile" ".bash_commons")
 backup_folder="./bak"
+clean_check="No need"
+backup_check="No need"
+install_check="Pass"
+bash_profile_check="Pass"
 
+
+if [ "$1" == "clean" ] || [ "$1" == "c" ] ; then
+    echo "*************************************************************************"
+    echo "Warning: Clean flag is set. Bakeup folder and build logs will be removed."
+    echo "*************************************************************************"
+fi
 echo "These files:"
 for file in ${files[@]}; do
     printf "%s\n" "$file"
 done
 
-read -rsp $'$will be backed up before installing. Still Install?[Y/N]\n' -n1 READ
+read -rsp $'will be backed up before installing. Still Install? [Y/N]\n' -n1 READ
 if [ "$READ" != 'y' ] && [ "$READ" != 'Y' ]; then
     echo "**** STOP doing this. ****"
     exit 1
 fi
 
+if [ "$1" == "clean" ] || [ "$1" == "c" ] ; then
+    echo "Remove $backup_folder and $log_file"
+    rm -rf $backup_folder
+    rm -rf $log_file
+    clean_check="Pass"
+fi
+
+exec > >(tee "$log_file") 2>&1
+
 check_folder_exist $backup_folder
 if [ $? -eq 1 ]; then
     echo "Start backing up..."
     mkdir $backup_folder
+    echo "Start copying files..."
     for file in ${files[@]}; do
         src_file=$HOME/$file
-        backup_files $src_file $backup_folder
+        copy_files $src_file $backup_folder
     done
+    echo "Finish copying files."
+    for file in ${files[@]}; do
+        src_file=$HOME/$file
+        bak_file=$backup_folder/$file
+        diff_files_folders $src_file $bak_file
+        diff_result=$(diff -r "$src_file" "$bak_file")
+        if [ $? -eq 0 ]; then
+            echo "Backup of '$src_file' was successful. Files are identical."
+        else
+            echo "Backup of '$src_file' failed. Files are different."
+            exit 2
+        fi
+    done
+    backup_check="Pass"
     echo "Finish backing up."
 fi
 
 
 for file in ${files[@]}; do
-    copy_files $file $HOME
+    copy_files ./$file $HOME
 done
+# Check the file is install or not
+for file in ${files[@]}; do
+    diff_files_folders ./$file $HOME/$file
+    if [ $? -eq 1 ]; then
+        install_check="Failed"
+    fi
+done
+
 # Setting bash_profile...
 echo "Setting bash_profile..."
 if grep -q "bash_commons" "$HOME/.bash_profile"; then
@@ -74,5 +121,20 @@ else
     if [ $? -eq 1 ]; then
         mkdir ~/bin
     fi
+    # Check bash_profile
+    if grep -q "bash_commons" "$HOME/.bash_profile"; then
+        bash_profile_check="Pass"
+    fi
 fi
 
+# Append timestamp in .bash_commons
+current_time=$(date +'%H:%M:%S')
+echo "# timestamp: $current_time" | tee -a ~/.bash_commons
+
+
+echo "********************************** Build Result ************************************"
+echo "Clean: $clean_check"
+echo "Backup: $backup_check"
+echo "Install: $install_check"
+echo "Bash_profile setup: $bash_profile_check"
+echo "************************************************************************************"
