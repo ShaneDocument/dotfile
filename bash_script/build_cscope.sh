c7d7d2f46cc4407328b1ac94e939a1bb70e0737b
ShaneDocument <shanelin@ShaneLin-NB.tw.smi.ad>
2025-06-27 12:30:05 +0800

    build_cscope.sh: Handle the path with space

--- bash_script/build_cscope.sh
+++ bash_script/build_cscope.sh
@@ -5,21 +5,23 @@
 # @folder: folder to find c and header files.
 
 check_file_exist() {
-    BASENAME=$(basename $1)
-    if [ ! -f $1 ]; then
+    FULLPATH="$1"
+    BASENAME=$(basename "$FULLPATH")
+    if [ ! -f "$FULLPATH" ]; then
         printf "%s: not found\n" "$BASENAME"
         exit 1
     fi
-    printf "[%s] :\n%s\n\n" "$BASENAME" "$1"
+    printf "[%s] :\n%s\n\n" "$BASENAME" "$FULLPATH"
 }
 
 check_folder_exist() {
-    BASENAME=$(basename $1)
-    if [ ! -d $1 ]; then
+    FULLPATH="$1"
+    BASENAME=$(basename "$FULLPATH")
+    if [ ! -d "$FULLPATH" ]; then
         printf "%s: not found or not a folder\n" "$BASENAME"
         exit 1
     fi
-    printf "[%s] :\n%s\n\n" "$BASENAME" "$1"
+    printf "[%s] :\n%s\n\n" "$BASENAME" "$FULLPATH"
 }
 
 PWD=$(pwd -P)
@@ -37,18 +39,26 @@ fi
 
 cscope_file_path=$PWD/cscope.files
 cscope_logs=$PWD/cscope.log
-$(rm $cscope_file_path > /dev/null 2> /dev/null)
-$(rm $cscope_logs > /dev/null 2> /dev/null)
+$(rm "$cscope_file_path")
+$(rm "$cscope_logs")
 for folder in "$@"
 do
-    if [ $folder == "rm" ]
+    if [ $folder == "-rm" ]
     then
         continue
     fi
     folder=$PWD/$folder
-    check_folder_exist $folder
-    echo $folder >> cscope_logs
-    find "$folder" \( -name build -o -name build_internal \) -prune -o -name "*.cfg" -o -name "*.c" -o -name "*.h" -o -name "*.json" -o -name "*.cpp" -o -name "*.hpp" >> $PWD/cscope.files
+    check_folder_exist "$folder"
+    echo $folder >> cscope.log
+    find "$folder" \
+        \( -name build -o -name build_internal \) -prune -o \
+        -name "*.cfg" -o \
+        -name "*.c" -o \
+        -name "*.h" -o \
+        -name "*.json" -o \
+        -name "*.cpp" -o \
+        -name "*.hpp" \
+        | awk '{print "\""$0"\""}' >> "$cscope_file_path"
 done
-check_file_exist $cscope_file_path
-cscope -Rqbvk
+check_file_exist "$cscope_file_path"
+cscope -Rqbvk -i "$cscope_file_path"
